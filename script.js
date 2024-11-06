39152ac0a96bca3be5a713405c6e72f3f00fa8bc
orangesaft <156236994+orangesaft@users.noreply.github.com>
2024-11-06 19:54:32 +0900

    thought it was right

--- script.js
+++ script.js
@@ -1,14 +1,17 @@
 //add input value on the ul
 const buttons = document.querySelector("button");
 const input = document.querySelector("#item");
+const shpli = document.querySelector("ul");
 
 //1.read input whenever button is triggered
-buttons.addEventListener("click", () => 
+buttons.addEventListener("click", () => {
 
-    console.log("input");
-   // function listcreator {
+    const newLi = document.createElement("li");
+    newLi.textContent = input.value; // Use input.value to get the value typed into the input field
 
 
-    );
+shpli.appendChild(newLi);
+
+    });
 //2.function that creates <li>newValue</li>
 //3.append to original ul
