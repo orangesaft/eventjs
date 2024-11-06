235e32b3aa28f9b26d0ee79829e596abc240a97d
orangesaft <156236994+orangesaft@users.noreply.github.com>
2024-11-06 19:34:52 +0900

    first thoughts

---
+++ script.js
@@ -0,0 +1,14 @@
+//add input value on the ul
+const buttons = document.querySelector("button");
+const input = document.querySelector("#item");
+
+//1.read input whenever button is triggered
+buttons.addEventListener("click", () => 
+
+    console.log("input");
+   // function listcreator {
+
+
+    );
+//2.function that creates <li>newValue</li>
+//3.append to original ul
